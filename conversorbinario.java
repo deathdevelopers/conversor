650046526884f022bcaeb1a54aab679e4fb229d4
Ruben Garcia <rubengarciaf27@gmail.com>
2016-03-14 11:44:46 +0100

    He agregado opcion octal

--- conversorbinario.java
+++ conversorbinario.java
@@ -55,4 +55,17 @@ public class Ejercicio04 {
 		    
 		    return binario;
 	}
+
+	static String octal(int num){
+		int acum=0;
+		String resultado="";
+		final char[] arrayOctal={'0', '1', '2', '3', '4', '5', '6', '7'};
+		while(num>0){
+			acum=num%8;
+			resultado=arrayOctal[acum]+resultado;
+			num/=8;
+		}
+		return resultado;
+	}
 }
+
